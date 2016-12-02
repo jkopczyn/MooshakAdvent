b04bec6477aaf0c830cedc67ffd29e44876ac56c
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-02 13:01:05 -0800

    yup, really trivial problems

---
+++ B.py
@@ -0,0 +1,18 @@
+import sys
+from collections import Counter
+
+order = []
+count = Counter()
+while(True):
+    item = raw_input()
+    item.strip()
+    if item == "stop":
+        break
+    count[item] += 1
+    if count[item] == 1:
+        order.append(item)
+for item in order:
+    if count[item] == 1:
+        print count[item], item
+    else:
+        print count[item], item+"s"
