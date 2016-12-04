17f66d0b60cba97968ab55ce5daea0cb13494d27
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-04 13:33:10 -0800

    checking if they're even close

--- C.py
+++ C.py
@@ -23,6 +23,14 @@ def coord(string):
     return [int(s) for s in string.split(',')]
 
 def check_intersection(first_line, second_line):
+    first_left, first_right = [coord[0] for coord in sorted(first_line)]
+    second_left, second_right = [coord[0] for coord in sorted(second_line)]
+    first_bottom, first_top = [coord[1] for coord in sorted(first_line, key=lambda x: x[1])]
+    second_bottom, second_top = [coord[1] for coord in sorted(second_line, key=lambda x: x[1])]
+    if first_left >= second_right or second_left >= first_right:
+        return False
+    if first_bottom >= second_top or second_bottom >= first_top:
+        return False
     return True
 
 print main()
