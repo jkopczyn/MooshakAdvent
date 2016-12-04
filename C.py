98d274311382c8fc46c62594fbe2a109d83a6e66
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-04 13:17:52 -0800

    basic structure of day 3

---
+++ C.py
@@ -0,0 +1,21 @@
+
+segments = []
+def main():
+    while(True):
+        item = raw_input().strip()
+        if not item:
+            break
+        if not segments:
+            first_coord = coord(item)
+            item = raw_input().strip()
+            second_coord = coord(item)
+        else:
+            second_coord = coord(item)
+            first_coord = segments[-1][1]
+        for pair in segments:
+            if check_intersection(pair, [first_coord, second_coord]):
+                return "crossing" 
+        segments.append([first_coord, second_coord])
+    return "not crossing"
+
+print main()
