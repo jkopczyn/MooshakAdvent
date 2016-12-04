49c731a70881d498c974e9914b4aaaacab2e92e6
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-04 13:23:32 -0800

    coord and sanity check

--- C.py
+++ C.py
@@ -12,6 +12,7 @@ def main():
         else:
             second_coord = coord(item)
             first_coord = segments[-1][1]
+        print first_coord, second_coord
         for pair in segments:
             if check_intersection(pair, [first_coord, second_coord]):
                 return "crossing" 
@@ -19,7 +20,7 @@ def main():
     return "not crossing"
 
 def coord(string):
-    return [0,0]
+    return [int(s) for s in string.split(',')]
 
 def check_intersection(first_line, second_line):
     return True
